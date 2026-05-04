15eda0c30cf3ba7d5ac85216ab7ddd2fde81b2ae
PhakanunK <phakanun1112094@gmail.com>
2026-05-04 18:07:39 +0700

    Connect to database
    Connect to database and use /health to check the connection.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.core.database import SessionLocal
 
-app = FastAPI()
+app = FastAPI(title="Mini ATS")
 
-@app.get("/")
-def read_root():
-    return {"message": "Backend is running!"}
+@app.get("/health")
+def health_check():
+    try:
+        db = SessionLocal()
+        db.execute(text("SELECT 1"))
+        db.close()
+        return {"status": "ok", "database": "connected"}
+    except Exception as e:
+        return {"status": "error", "database": str(e)}
