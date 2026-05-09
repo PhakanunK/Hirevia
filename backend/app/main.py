23f0e63ccad3ff0865d6c3540d8f0b6ceaaa66d0
PhakanunK <phakanun1112094@gmail.com>
2026-05-09 23:12:13 +0700

    Add CORS
    Add CORS to main and add debug setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from app.core.database import SessionLocal
+from app.core.config import settings
 from app.api.v1.admin import auth, users
 
-app = FastAPI(title="Mini ATS")
+app = FastAPI(
+    title="HireFlow ATS",
+    docs_url="/docs" if settings.DEBUG else None,
+    redoc_url="/redoc" if settings.DEBUG else None
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/admin/auth", tags=["Admin Auth"])
 app.include_router(users.router, prefix="/admin/users", tags=["Admin Users"])
